058b0356dbb0b31048084aa58ba5d7596d6283a0
superrediska <rediskaqq@yandex.ru>
2025-05-10 12:07:16 +0300

    task1415

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1415/MyStack.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task14.task1415;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Стек в домашних условиях
+*/
+
+public class MyStack {
+
+    private final List<String> storage = new LinkedList<>();
+
+    public void push(String s) {
+        storage.add(0, s);
+    }
+
+    public String pop() {
+        if (storage.isEmpty()) {
+            return null;
+        }
+        return storage.remove(0);
+    }
+
+    public String peek() {
+        if (storage.isEmpty()) {
+            return null;
+        }
+        return storage.get(0);
+    }
+
+    public boolean empty() {
+        return storage.isEmpty();
+    }
+
+    public int search(String s) {
+        return storage.indexOf(s);
+    }
+}
