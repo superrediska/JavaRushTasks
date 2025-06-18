a7d43ed7b207ac9d98908ffa4b3e55078ffb40da
superrediska <rediskaqq@yandex.ru>
2025-06-18 19:44:48 +0300

    task1332

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1332/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task13.task1332;
+
+/*
+Вирусы в Инстаматрице
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public static Set<String> matrixLogs = new HashSet<>();
+
+    public static void main(String[] args) {
+        fillMatrixLog();
+        System.out.println(matrixLogs);
+        removeBugs();
+        System.out.println(matrixLogs);
+    }
+
+    public static void fillMatrixLog() {
+        for (int i = 0; i < 400; i++) {
+            matrixLogs.add("interestingLog" + i + ".log");
+            if ((i > 0) && (i % 5 == 0) && (i < 295)) {
+                matrixLogs.add("buahaha" + i + ".bug");
+            }
+        }
+    }
+
+    public static void removeBugs(){
+
+        for (String matrixLog :new HashSet<>(matrixLogs)) {
+            if(matrixLog.startsWith("buahaha")){
+                matrixLogs.remove(matrixLog);
+            }
+        }
+    }
+}
