04d39e6ba026e4ac15fc16c95a703690ea73a362
superrediska <rediskaqq@yandex.ru>
2025-07-09 14:37:18 +0300

    JC_task1217

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface  CanFly{
+        void print();
+    }
+    public interface CanRun{
+        void print();
+    }
+    public interface CanSwim{
+        void print();
+    }
+
+}
