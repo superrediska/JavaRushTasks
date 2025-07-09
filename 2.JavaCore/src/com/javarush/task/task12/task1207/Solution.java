5295583c5b598bde8b4e27fbc703126a81fd4224
superrediska <rediskaqq@yandex.ru>
2025-07-09 13:22:48 +0300

    JC_task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(3);
+        print(null);
+    }
+
+    public static void print(int i){}
+    public static void print(Integer b){}
+}
