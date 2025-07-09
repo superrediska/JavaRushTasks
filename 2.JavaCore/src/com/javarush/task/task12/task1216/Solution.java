4578ccab06b6922d91a48d30d26e2591c046b1a4
superrediska <rediskaqq@yandex.ru>
2025-07-09 14:35:33 +0300

    JC_task1216

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public interface CanFly{
+        void print();
+        void getName();
+    }
+
+}
