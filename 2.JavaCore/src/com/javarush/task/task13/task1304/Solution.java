e3c1dcc46b78a92b13cbd651d0e1ec210365bdfc
superrediska <rediskaqq@yandex.ru>
2025-07-28 11:40:19 +0300

    JC_task1304

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1304/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task13.task1304;
+
+/* 
+Selectable и Updatable
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+    public static abstract class Screen implements Selectable, Updatable{
+
+    }
+}
