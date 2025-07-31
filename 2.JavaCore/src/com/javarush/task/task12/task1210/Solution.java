bf475e41a94c58b8b7ad1f25678a78c0ddcb4235
superrediska <rediskaqq@yandex.ru>
2025-07-31 15:06:39 +0300

    JC_task1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static int max(int a, int b){
+        return Integer.max(a, b);
+    }
+    public static long max(long a, long b){
+        return Long.max(a, b);
+    }
+    public static double max(double a, double b){
+        return Double.max(a, b);
+    }
+
+}
