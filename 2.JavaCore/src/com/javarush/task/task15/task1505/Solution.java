ce1b9a6816aeb96ba69d9a4fa99ca81e23c2bf49
superrediska <rediskaqq@yandex.ru>
2025-08-02 19:04:28 +0300

    JC_task1505

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1505/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task15.task1505;
+
+/* 
+ООП - исправь ошибки в наследовании
+*/
+
+public class Solution {
+    public interface HasWeight {
+        int getWeight();
+    }
+
+    public interface HasHeight {
+        int getHeight();
+    }
+
+    public static class Human implements HasWeight, HasHeight {
+        public int getWeight(){
+
+            return 1;
+        }
+        public int getHeight(){
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.getWeight());
+        System.out.println(human.getHeight());
+    }
+}
