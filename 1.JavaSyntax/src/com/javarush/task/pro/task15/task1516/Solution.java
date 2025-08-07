5605e092c5a0637130eb1a6f60ad951d0b69ad23
superrediska <rediskaqq@yandex.ru>
2025-08-07 10:51:26 +0300

    JC_task1516

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1516/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task15.task1516;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Файл или директория
+*/
+
+public class Solution {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Path path = Path.of(scan.nextLine());
+        while (Files.exists(path)) {
+            if (Files.isRegularFile(path)) {
+                System.out.println(path + THIS_IS_FILE);
+            }
+            if (Files.isDirectory(path)) {
+                System.out.println(path + THIS_IS_DIR);
+            } else {
+                break;
+            }
+            path = Path.of(scan.nextLine());
+        }
+    }
+}
+
