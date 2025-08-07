7c993d2d09111923a75bae181d90d16a5fcd64e6
superrediska <rediskaqq@yandex.ru>
2025-08-07 21:39:08 +0300

    JC_task1516

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1516/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task15.task1516;
+
+/* 
+Значения по умолчанию
+*/
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Solution {
+
+    public int intVar;
+    public double doubleVar;
+    public Double DoubleVar;
+    public boolean booleanVar;
+    public Object ObjectVar;
+    public Exception ExceptionVar;
+    public String StringVar;
+
+    public static void main(String[] args) {
+        System.out.println(0);
+        System.out.println(0.0);
+        System.out.println("null");
+        System.out.println(false);
+        System.out.println("null");
+        System.out.println("null");
+        System.out.println("null");
+
+    }
+
+}
