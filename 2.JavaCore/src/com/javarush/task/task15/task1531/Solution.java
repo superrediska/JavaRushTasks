9dd90d1ea2f2d9907753c117280f0efd6c14172b
superrediska <rediskaqq@yandex.ru>
2025-08-08 22:20:22 +0300

    JC_task1531

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task15.task1531;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+
+/* 
+Факториал
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        System.out.println(factorial(input));
+    }
+
+    public static String factorial(int n) {
+        BigDecimal dec = new BigDecimal(1);
+        if ( n < 0){
+            return "0";
+        }
+        if (n == 0 || n == 1){
+            return "1";
+        }
+        for (int i = 1; i <= n ; i++) {
+            dec = dec.multiply(BigDecimal.valueOf(i));
+        }
+        return String.valueOf(dec);
+
+    }
+}
