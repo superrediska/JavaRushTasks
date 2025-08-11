809159119063de9dee286fd3c3cc7e34c69dea62
superrediska <rediskaqq@yandex.ru>
2025-08-11 20:42:55 +0300

    JC_task1303, JC_task1306, JC_task1311, JC_task1318, JC_task1320, JC_task1324

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1324/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task13.task1324;
+
+import java.awt.*;
+
+/* 
+Один метод в классе
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+
+    public static abstract class Fox {
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
