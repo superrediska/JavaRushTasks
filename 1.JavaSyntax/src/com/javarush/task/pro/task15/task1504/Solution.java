0682f400b1c0c2ccde0d89b3c71f2bc492dd7b95
superrediska <rediskaqq@yandex.ru>
2025-08-11 20:49:35 +0300

    task1504, task1509, task1510, task1511

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -12,27 +12,8 @@ import java.util.Scanner;
 */
 
 public class Solution {
-
-    public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        String fileName1 = scan.nextLine();
-        String fileName2 = scan.nextLine();
-        try (InputStream inputStream = Files.newInputStream(Paths.get(fileName1));
-             OutputStream outputStream = Files.newOutputStream(Paths.get(fileName2))
-        ) {
-            byte[] bytes = inputStream.readAllBytes();
-            for (int i = 0; i < bytes.length; i += 2) {
-                if ( i < bytes.length - 1) {
-                    byte temp = bytes[i];
-                    bytes[i] = bytes[i + 1];
-                    bytes[i + 1] = temp;
-                }
-            }
-            outputStream.write(bytes);
-        }
-
+    public static void main(String[] args) {
 
     }
-
 }
 
