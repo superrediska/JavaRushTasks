8a4230654eef1be2b12051ce86b4adb89a9a96ae
superrediska <rediskaqq@yandex.ru>
2025-08-11 20:43:53 +0300

    JC_task1407, JC_task1409, JC_task1412, JC_task1415

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1412/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task14.task1412;
+
+/* 
+Реализовать метод printMainInfo
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Object obj = new Circle();
+        Movable movable = (Movable) obj;
+        Drawable drawable = new Rectangle();
+
+        printMainInfo(drawable);
+        printMainInfo(movable);
+    }
+
+    public static void printMainInfo(Object object) {
+        if (object.getClass().getName().equals("Solution$Circle")) {
+            ((Movable) object).move();}
+        else if (object.getClass().getName().equals("Solution$Rectangle"))
+            ((Drawable) object).draw();
+    }
+    }
+
+    interface Movable {
+
+        void move();
+    }
+
+    class Circle implements Movable {
+
+        public void draw() {
+            System.out.println("Can be drawn");
+        }
+
+        public void move() {
+            System.out.println("Can be moved");
+        }
+
+    }
+
+    interface Drawable {
+        void draw();
+    }
+
+    class Rectangle implements Drawable {
+        public void draw() {
+            System.out.println("Can be drawn");
+        }
+
+        public void move() {
+            System.out.println("Can be moved");
+        }
+    }
+
