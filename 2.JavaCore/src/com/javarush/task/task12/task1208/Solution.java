49d41eeef1c7e76a265c6371cb13c1c74a0bb5cb
superrediska <rediskaqq@yandex.ru>
2025-08-12 20:09:50 +0300

    JC_task1208

--- 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -5,11 +5,13 @@ package com.javarush.task.task12.task1208;
 */
 
 public class Solution {
-    public static void print(String[] args) {
+    public static void main(String[] args) {
 
+    }
+    public static void print(Character a) {
     }
     public static void print(int b){}
     public static void print(Integer a){}
     public static void print(String c){}
-    public void print(char a){}
+    public static void print(char a){}
 }
