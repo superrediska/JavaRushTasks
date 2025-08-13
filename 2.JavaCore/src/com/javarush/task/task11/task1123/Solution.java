af82f0f0abd05a011bfd9f752f6899fe61be052b
superrediska <rediskaqq@yandex.ru>
2025-08-13 15:19:33 +0300

    JC_task1123

--- 2.JavaCore/src/com/javarush/task/task11/task1123/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task11/task1123/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task11.task1123;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
@@ -15,9 +17,10 @@ public class Solution {
             return new Pair(null, null);
         }
 
-        // напишите тут ваш код
+        int x = Arrays.stream(inputArray).min().getAsInt();
+        int y = Arrays.stream(inputArray).max().getAsInt();
 
-        return new Pair(0, 0);
+        return new Pair(x, y);
     }
 
     public static class Pair {
