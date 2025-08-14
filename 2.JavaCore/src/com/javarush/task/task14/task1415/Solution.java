f0ac55c7bd3b13e21e3ce3eec3a905cbd3da3057
superrediska <rediskaqq@yandex.ru>
2025-08-14 21:11:58 +0300

    JC_task1415

--- 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
@@ -1,48 +1,57 @@
-//package com.javarush.task.task14.task1415;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///*
-//Клининговый центр
-//*/
-//
-//public class Solution {
-//    public static void main(String[] args) {
-//        List<Apartment> apartments = new ArrayList<Apartment>();
-//        apartments.add(new OneRoomApt());
-//        apartments.add(new TwoRoomApt());
-//        apartments.add(new ThreeRoomApt());
-//
-//        cleanAllApartments(apartments);
-//    }
-//
-//    public static void cleanAllApartments(List<Apartment> apartments) {
-//        for (Apartment apartment : apartments){
-//            if (apartment instanceof OneRoomApt){
-//                OneRoomApt
-//            }
-//        }
-//    }
-//
-//    static interface Apartment {
-//    }
-//
-//    static class OneRoomApt implements Apartment {
-//        void clean1Room() {
-//            System.out.println("1 room is cleaned");
-//        }
-//    }
-//
-//    static class TwoRoomApt implements Apartment {
-//        void clean2Rooms() {
-//            System.out.println("2 rooms are cleaned");
-//        }
-//    }
-//
-//    static class ThreeRoomApt implements Apartment {
-//        void clean3Rooms() {
-//            System.out.println("3 rooms are cleaned");
-//        }
-//    }
-//}
+package com.javarush.task.task14.task1415;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Клининговый центр
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Apartment> apartments = new ArrayList<Apartment>();
+        apartments.add(new OneRoomApt());
+        apartments.add(new TwoRoomApt());
+        apartments.add(new ThreeRoomApt());
+
+        cleanAllApartments(apartments);
+    }
+
+    public static void cleanAllApartments(List<Apartment> apartments) {
+        for (Apartment str : apartments){
+            if (str instanceof OneRoomApt){
+                OneRoomApt one = (OneRoomApt) str;
+                one.clean1Room();
+            }
+            else if (str instanceof TwoRoomApt){
+                TwoRoomApt two = (TwoRoomApt) str;
+                two.clean2Rooms();
+            }
+            else if (str instanceof ThreeRoomApt){
+                ThreeRoomApt three = (ThreeRoomApt) str;
+                three.clean3Rooms();
+            }
+        }
+    }
+
+    static interface Apartment {
+    }
+
+    static class OneRoomApt implements Apartment {
+        void clean1Room() {
+            System.out.println("1 room is cleaned");
+        }
+    }
+
+    static class TwoRoomApt implements Apartment {
+        void clean2Rooms() {
+            System.out.println("2 rooms are cleaned");
+        }
+    }
+
+    static class ThreeRoomApt implements Apartment {
+        void clean3Rooms() {
+            System.out.println("3 rooms are cleaned");
+        }
+    }
+}
