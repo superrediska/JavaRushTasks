70cb4db2e6e67838f92bc3c6f7f92c5b240c0072
superrediska <rediskaqq@yandex.ru>
2025-08-15 21:27:11 +0300

    JC_task1512

--- 2.JavaCore/src/com/javarush/task/task15/task1512/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1512/Solution.java
@@ -11,20 +11,15 @@ public class Solution {
     }
 
     public static class NakedCat {
-        public NakedCat() {
-            super();
-        }
     }
 
     public static class NormalCat extends NakedCat {
-        public NormalCat() {
-            super();
-        }
-
         public NormalCat(String name) {
             System.out.println("My name is " + name);
         }
+        public NormalCat() {
 
+        }
         public NakedCat shave() {
             return this;
         }
