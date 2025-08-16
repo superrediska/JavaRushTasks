3ececb46ca9a7aa747c3f0318c9702c31ebfeba0
superrediska <rediskaqq@yandex.ru>
2025-08-16 15:55:51 +0300

    JC_task1408

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/Hen.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task14.task1408;
+
+abstract public class Hen {
+    abstract public int getCountOfEggsPerMonth();
+    public String getDescription(){
+        return "Я - курица.";
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/RussianHen.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task14.task1408;
+
+public class RussianHen extends Hen {
+   public int getCountOfEggsPerMonth(){
+       return 10;
+   }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription()+" Моя страна - " + Country.RUSSIA + ". Я несу " + getCountOfEggsPerMonth() + " яиц в месяц.";
+    }
+}
