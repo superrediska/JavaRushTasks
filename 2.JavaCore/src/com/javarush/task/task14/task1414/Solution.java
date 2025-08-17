e7d7b9a14368b64c9a3551126eb6d730404c3736
superrediska <rediskaqq@yandex.ru>
2025-08-17 21:53:13 +0300

    JC_task1414

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task14.task1414;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+MovieFactory
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            Movie movie = MovieFactory.getMovie(scan.nextLine());
+            if (movie != null)
+                System.out.println(movie.getClass().getSimpleName());
+            else
+                break;
+        }
+    }
+
+
+    //ввести с консоли несколько ключей (строк), пункт 7
+
+        /*
+8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
+8.2 вывести на экран movie.getClass().getSimpleName()
+        */
+
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+            if ("soapOpera".equals(key)) {
+                movie = new SoapOpera();
+            }
+            if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            }
+            if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
+
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie {
+
+    }
+
+    static class Thriller extends Movie {
+
+    }
+
+}
