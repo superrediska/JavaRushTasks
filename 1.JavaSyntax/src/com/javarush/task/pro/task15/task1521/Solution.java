517e71b3729e833ff41a7be9680ae54ae42fd020
superrediska <rediskaqq@yandex.ru>
2025-08-19 20:56:29 +0300

    task1521

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1521/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task15.task1521;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Временное сохранение файла
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        URL url = new URL(scanner.nextLine());
+        InputStream input = url.openStream();
+        Path tempPath = Files.createTempFile(null,null);
+        byte[] buffer = input.readAllBytes();
+        Files.write(tempPath,buffer);
+    }
+}
