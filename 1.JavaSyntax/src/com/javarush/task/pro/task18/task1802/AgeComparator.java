3d84971e3f8a74f7684b5d4a03fb960d7cab8b29
superrediska <rediskaqq@yandex.ru>
2025-08-21 21:05:14 +0300

    task1802

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1802/AgeComparator.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task18.task1802;
+
+import java.util.Comparator;
+
+/* 
+Сортировка по возрасту
+*/
+
+public class AgeComparator implements Comparator<Student>{
+    public int compare(Student student1, Student student2){
+        if (student1.getAge() > student2.getAge()){
+            return -1;
+        }
+        if (student1.getAge() < student2.getAge()){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
