ed8b49ea76646e8f6fb51cc0fbb0f1c97696aca2
superrediska <rediskaqq@yandex.ru>
2025-08-23 16:16:43 +0300

    task1504

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -13,7 +13,23 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        try (InputStream input = Files.newInputStream(Paths.get(scan.nextLine()));
+             OutputStream output = Files.newOutputStream(Paths.get(scan.nextLine()))) {
+            byte[] buffer = input.readAllBytes();
+            byte[] Buffer1 = new byte[buffer.length];
+            for (int i = 0; i < buffer.length; i += 2) {
+                if (buffer.length - 1 == i && i % 2 == 0) {
+                    Buffer1[i] = buffer[i];
+                } else {
+                    Buffer1[i] = buffer[i + 1];
+                    Buffer1[i + 1] = buffer[i];
+                }
+            }
+            output.write(Buffer1);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
 
