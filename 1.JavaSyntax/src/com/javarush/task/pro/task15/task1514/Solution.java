a8929b178076c9b53a69c5958128ea64ae0b87aa
superrediska <rediskaqq@yandex.ru>
2025-08-24 14:17:52 +0300

    task1514

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1514/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task15.task1514;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Все относительно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String str1 = scanner.nextLine();
+            String str2 = scanner.nextLine();
+            Path path1 = Path.of(str1);
+            Path path2 = Path.of(str2);
+            boolean bool = Path.of(str1).isAbsolute();
+            boolean bool1 = Path.of(str2).isAbsolute();
+            if(bool == false || bool1 == false){
+                Path path = path1.relativize(path2);
+                System.out.println(path);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
+
