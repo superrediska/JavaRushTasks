2cd7d80231df6f43f6d1b5083ec5c5d0746e863b
superrediska <rediskaqq@yandex.ru>
2025-08-24 21:34:22 +0300

    task1510

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.pro.task15.task1510;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
@@ -10,17 +11,17 @@ import java.util.Scanner;
 /* 
 Пишем байты в файл
 */
-//
-//public class Solution {
-//    public static void main(String[] args) throws IOException {
-//        byte[] bytes = args[0].getBytes();
-//        try (InputStream stream = System.in;
-//             Scanner scanner = new Scanner(stream);
-//             BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(scanner.nextLine()))) {
-//            bufferedWriter.write(bytes);
-//        } catch (IOException e) {
-//            System.out.println("Something went wrong : " + e);
-//        }
-//    }
-//}
-//
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = args[0].getBytes();
+        try (InputStream stream = System.in;
+             Scanner scanner = new Scanner(stream);
+             OutputStream bufferedOutputStream = Files.newOutputStream(Files.write(Path.of(scanner.nextLine()), bytes))) {
+             bufferedOutputStream.write(bytes);
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
+
