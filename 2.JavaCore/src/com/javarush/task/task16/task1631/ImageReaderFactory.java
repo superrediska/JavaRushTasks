17a8ae2b972e968cc68649eb81cf7cdafaa56af1
superrediska <rediskaqq@yandex.ru>
2025-10-13 17:31:28 +0300

    task1631

--- 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -1,21 +1,20 @@
 package com.javarush.task.task16.task1631;
 
 import com.javarush.task.task16.task1631.common.*;
-
 public class ImageReaderFactory {
     public static ImageReader getImageReader(ImageTypes imageTypes) {
         if (imageTypes == null){
             throw new IllegalArgumentException("Неизвестный тип картинки");
         }
-        if (imageTypes == ImageTypes.JPG) {
-            return new JpgReader();
+
+        if (imageTypes == ImageTypes.BMP){
+            return new BmpReader();
         } else if (imageTypes == ImageTypes.PNG) {
             return new PngReader();
-        } else if (imageTypes == ImageTypes.BMP) {
-            return new BmpReader();
-        }else {
+        } else if (imageTypes == ImageTypes.JPG) {
+            return new JpgReader();
+        } else {
             throw new IllegalArgumentException("Неизвестный тип картинки");
         }
-
     }
 }
