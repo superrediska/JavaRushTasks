a0924ff68081b9b48af15b4b2b5ba265ad5961b4
superrediska <rediskaqq@yandex.ru>
2026-07-23 11:55:47 +0300

    JM_task2211

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -9,5 +9,14 @@ import java.nio.charset.StandardCharsets;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "Windows-1251"));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), StandardCharsets.UTF_8))) {
+            String str;
+            while ((str = bufferedReader.readLine()) != null){
+                bufferedWriter.write(str);
+            }
+        }
     }
+
 }
